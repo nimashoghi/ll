0e158660662b480caac307709a1e9a818e1e76cb
Nima Shoghi <nimashoghi@gmail.com>
2023-08-24 23:46:02 +0000

    Update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
-mamba create -n fm python=3.11
+mamba create -n fm python=3.10
 mamba activate fm
+
 mamba install -y \
-    -c conda-forge -c pytorch -c nvidia -c pyg \
+    -c pytorch -c nvidia -c pyg \
+    pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 \
+    pyg pytorch-scatter pytorch-sparse pytorch-cluster
+
+mamba install -y \
+    -c conda-forge \
     numpy matplotlib seaborn sympy pandas numba scikit-learn ipykernel ipywidgets tqdm pyyaml networkx \
-    pytorch torchvision torchaudio pytorch-cuda=11.8 \
-    pyg pytorch-scatter pytorch-cluster \
-    pytorch-lightning torchmetrics \
+    pytorch-lightning torchmetrics lightning \
     wandb \
     ase python-lmdb h5py \
     cloudpickle \
