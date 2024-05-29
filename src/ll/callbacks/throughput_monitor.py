77c6ce54e36ef94febcecfb758c9cac86d3922ce
Nima Shoghi <nimashoghi@gmail.com>
2024-05-28 20:33:20 -0400

    Update

--- src/ll/callbacks/throughput_monitor.py
+++ src/ll/callbacks/throughput_monitor.py
@@ -1,7 +1,7 @@
 import time
 from abc import ABC, abstractmethod
 from logging import getLogger
-from typing import TYPE_CHECKING, Any, Literal, Protocol, cast, runtime_checkable
+from typing import TYPE_CHECKING, Any, Literal
 
 import torch
 from lightning.pytorch.callbacks.throughput_monitor import (
@@ -25,45 +25,7 @@ class CallbackBaseConfig(TypedConfig, ABC):
     def construct_callback(self) -> "Callback": ...
 
 
-@runtime_checkable
-class ThroughputMonitorableWithBatchSizeFnModule(Protocol):
-    def throughput_monitor_compute_batch_size(self, batch: Any) -> int: ...
-
-
-@runtime_checkable
-class ThroughputMonitorableModuleWithLengthFn(Protocol):
-    def throughput_monitor_compute_length(self, batch: Any) -> int: ...
-
-
 class ThroughputMonitorCallback(_ThroughputMonitor):
-    @override
-    @override
-    def setup(
-        self, trainer: "Trainer", pl_module: "LightningModule", stage: str
-    ) -> None:
-        super().setup(trainer, pl_module, stage)
-
-        # If batch_size_fn is not provided, we can use the default one.
-        if self.batch_size_fn is None:
-            if not isinstance(pl_module, ThroughputMonitorableWithBatchSizeFnModule):
-                raise ValueError(
-                    f"Module {type(pl_module)} does not implement throughput_monitor_compute_batch_size. "
-                    "See the ThroughputMonitorableWithBatchSizeFnModule protocol."
-                )
-
-            self.batch_size_fn = pl_module.throughput_monitor_compute_batch_size
-
-        # If length_fn is not provided, we can use the default one.
-        if self.length_fn is None:
-            if not isinstance(pl_module, ThroughputMonitorableModuleWithLengthFn):
-                rank_zero_warn(
-                    f"Module {type(pl_module)} does not implement throughput_monitor_compute_length. "
-                    "See the ThroughputMonitorableModuleWithLengthFn protocol. "
-                    "Length function will not be used."
-                )
-            else:
-                self.length_fn = pl_module.throughput_monitor_compute_length
-
     @override
     def _update(
         self,
@@ -110,6 +72,12 @@ class ThroughputMonitorCallback(_ThroughputMonitor):
 class ThroughputMonitorConfig(CallbackBaseConfig):
     name: Literal["throughput_monitor"] = "throughput_monitor"
 
+    batch_size: int
+    """Batch size to use for computing throughput."""
+
+    length: int | None = None
+    """Length to use for computing throughput."""
+
     window_size: int = 100
     """Number of batches to use for a rolling average."""
 
@@ -119,37 +87,8 @@ class ThroughputMonitorConfig(CallbackBaseConfig):
     @override
     def construct_callback(self):
         return ThroughputMonitorCallback(
-            batch_size_fn=cast(Any, None),  # None is okay here
-            length_fn=None,
+            batch_size_fn=lambda _: self.batch_size,
+            length_fn=(lambda _: l) if (l := self.length) is not None else None,
             window_size=self.window_size,
             separator=self.separator,
         )
-
-    @classmethod
-    def from_pyg_batch(cls):
-        try:
-            from torch_geometric.data import Batch
-        except ImportError:
-            raise ImportError(
-                f"{cls.__name__}.from_pyg_batch requires torch-geometric to be installed."
-            )
-
-        def batch_size_fn(batch: Batch):
-            return batch.num_graphs
-
-        def length_fn(batch: Batch):
-            if (num_nodes := getattr(batch, "num_nodes", None)) is not None:
-                assert isinstance(
-                    num_nodes, int
-                ), f"num_nodes must be an int, got {type(num_nodes)=}"
-                return num_nodes
-
-            num_nodes = 0
-            for graph in batch.to_data_list():
-                assert (
-                    graph.num_nodes is not None
-                ), "num_nodes must be set for all graphs"
-                num_nodes += graph.num_nodes
-            return num_nodes
-
-        return cls(batch_size_fn=batch_size_fn, length_fn=length_fn)
