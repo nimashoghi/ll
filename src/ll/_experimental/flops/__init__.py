fae15d5b5f5ff8ef6add90d8fad05150639fc218
Nima Shoghi <nimashoghi@gmail.com>
2024-06-08 02:02:20 -0400

    Update

--- src/ll/_experimental/flops/__init__.py
+++ src/ll/_experimental/flops/__init__.py
@@ -1,7 +1,6 @@
 from collections.abc import Callable
 
 import torch
-from lightning.fabric.utilities.imports import _TORCH_GREATER_EQUAL_2_1
 
 
 def measure_flops(
@@ -32,8 +31,6 @@ def measure_flops(
             FLOPs will be included in the result.
 
     """
-    if not _TORCH_GREATER_EQUAL_2_1:
-        raise ImportError("`measure_flops` requires PyTorch >= 2.1.")
     from .flop_counter import FlopCounterMode
 
     flop_counter = FlopCounterMode(display=display)
--- src/ll/callbacks/throughput_monitor.py
+++ src/ll/callbacks/throughput_monitor.py
@@ -1,77 +1,28 @@
-import time
 from logging import getLogger
-from typing import TYPE_CHECKING, Any, Literal
+from typing import TYPE_CHECKING, Literal
 
-import torch
-from lightning.fabric.utilities.rank_zero import rank_zero_warn
 from lightning.pytorch.callbacks.callback import Callback
 from typing_extensions import override
 
 from .base import CallbackConfigBase
 
-if TYPE_CHECKING:
-    from lightning.pytorch import LightningModule, Trainer
-    from lightning.pytorch.callbacks.callback import Callback
-
-
 log = getLogger(__name__)
 
 
 try:
-    from lightning.pytorch.callbacks.throughput_monitor import (
-        ThroughputMonitor as _ThroughputMonitor,
-    )
+    from ._throughput_monitor_callback import ThroughputMonitor
 
-    class ThroughputMonitorCallback(_ThroughputMonitor):  # pyright: ignore[reportRedeclaration]
-        @override
-        def _update(
-            self,
-            trainer: "Trainer",
-            pl_module: "LightningModule",
-            batch: Any,
-            iter_num: int,
-        ) -> None:
-            stage = trainer.state.stage
-            assert stage is not None
-            throughput = self._throughputs[stage]
-
-            if trainer.strategy.root_device.type == "cuda":
-                # required or else perf_counter() won't be correct
-                torch.cuda.synchronize()
-
-            elapsed = time.perf_counter() - self._t0s[stage]
-            if self.length_fn is not None:
-                with torch.inference_mode():
-                    length = self.length_fn(batch)
-                self._lengths[stage] += length
-
-            if hasattr(pl_module, "flops_per_batch"):
-                flops_per_batch = pl_module.flops_per_batch
-            else:
-                rank_zero_warn(
-                    "When using the `ThroughputMonitor`, you need to define a `flops_per_batch` attribute or property"
-                    f" in {type(pl_module).__name__} to compute the FLOPs."
-                )
-                flops_per_batch = None
-
-            with torch.inference_mode():
-                batch_size = self.batch_size_fn(batch)
-            throughput.update(
-                time=elapsed,
-                batches=iter_num,
-                # this assumes that all iterations used the same batch size
-                samples=iter_num * batch_size,
-                lengths=None if self.length_fn is None else self._lengths[stage],
-                flops=flops_per_batch,
-            )
 
 except ImportError:
+    if TYPE_CHECKING:
+        from ._throughput_monitor_callback import ThroughputMonitor
+    else:
 
-    class ThroughputMonitorCallback(Callback):
-        def __init__(self, *args, **kwargs):
-            raise ImportError(
-                "To use the `ThroughputMonitorCallback`, please install the `lightning` package."
-            )
+        class ThroughputMonitor(Callback):
+            def __init__(self, *args, **kwargs):
+                raise ImportError(
+                    "To use the `ThroughputMonitor`, please install the `lightning` package."
+                )
 
 
 class ThroughputMonitorConfig(CallbackConfigBase):
@@ -91,7 +42,7 @@ class ThroughputMonitorConfig(CallbackConfigBase):
 
     @override
     def construct_callbacks(self, root_config):
-        yield ThroughputMonitorCallback(
+        yield ThroughputMonitor(
             batch_size_fn=lambda _: self.batch_size,
             length_fn=(lambda _: l) if (l := self.length) is not None else None,
             window_size=self.window_size,
