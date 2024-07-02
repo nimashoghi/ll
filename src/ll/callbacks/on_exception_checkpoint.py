3c5d4bd42b5b1bd4cc4c5b1da0f68701ea9c9bfc
Nima Shoghi <nimashoghi@gmail.com>
2024-07-01 20:09:33 -0400

    Disable barrier for onexceptioncheckpoint save

--- src/ll/callbacks/on_exception_checkpoint.py
+++ src/ll/callbacks/on_exception_checkpoint.py
@@ -1,7 +1,9 @@
 import datetime
 import logging
 import os
+from typing import Any
 
+from lightning.pytorch import Trainer
 from lightning.pytorch.callbacks import OnExceptionCheckpoint as _OnExceptionCheckpoint
 from typing_extensions import override
 
@@ -18,3 +20,25 @@ class OnExceptionCheckpoint(_OnExceptionCheckpoint):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         ckpt_path, ext = os.path.splitext(ckpt_path)
         return f"{ckpt_path}_{timestamp}{ext}"
+
+    @override
+    def on_exception(self, trainer: Trainer, *_: Any, **__: Any) -> None:
+        # We override this to checkpoint the model manually,
+        # without calling the dist barrier.
+
+        # trainer.save_checkpoint(self.ckpt_path)
+
+        if trainer.model is None:
+            raise AttributeError(
+                "Saving a checkpoint is only possible if a model is attached to the Trainer. Did you call"
+                " `Trainer.save_checkpoint()` before calling `Trainer.{fit,validate,test,predict}`?"
+            )
+        checkpoint = trainer._checkpoint_connector.dump_checkpoint(weights_only=False)
+        trainer.strategy.save_checkpoint(
+            checkpoint, self.ckpt_path, storage_options=None
+        )
+        # self.strategy.barrier("Trainer.save_checkpoint") # <-- This is disabled
+
+    @override
+    def teardown(self, trainer: Trainer, *_: Any, **__: Any) -> None:
+        trainer.strategy.remove_checkpoint(self.ckpt_path)
