a9b05a2335edd94d5d71be1260a973fce6054932
Nima Shoghi <nimashoghi@gmail.com>
2024-07-16 16:03:02 -0400

    Fix BF16 dtype on V100

--- src/ll/trainer/trainer.py
+++ src/ll/trainer/trainer.py
@@ -29,6 +29,30 @@ from .signal_connector import _SignalConnector
 log = logging.getLogger(__name__)
 
 
+def _is_bf16_supported_no_emulation():
+    r"""Return a bool indicating if the current CUDA/ROCm device supports dtype bfloat16."""
+    version = cast(Any, torch.version)
+
+    # Check for ROCm, if true return true, no ROCM_VERSION check required,
+    # since it is supported on AMD GPU archs.
+    if version.hip:
+        return True
+
+    device = torch.cuda.current_device()
+
+    # Check for CUDA version and device compute capability.
+    # This is a fast way to check for it.
+    cuda_version = version.cuda
+    if (
+        cuda_version is not None
+        and int(cuda_version.split(".")[0]) >= 11
+        and torch.cuda.get_device_properties(device).major >= 8
+    ):
+        return True
+
+    return False
+
+
 class Trainer(LightningTrainer):
     @classmethod
     @contextlib.contextmanager
@@ -163,7 +187,7 @@ class Trainer(LightningTrainer):
                     try:
                         resolved_precision = (
                             "bf16-mixed"
-                            if torch.cuda.is_bf16_supported()
+                            if _is_bf16_supported_no_emulation()
                             else "16-mixed"
                         )
                     except BaseException:
