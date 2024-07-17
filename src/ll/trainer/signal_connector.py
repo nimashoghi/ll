fcffe32d84b37cfe27a63d60a5cb118d8f3ca6db
Nima Shoghi <nimashoghi@gmail.com>
2024-07-17 11:39:42 -0400

    Update

--- src/ll/trainer/signal_connector.py
+++ src/ll/trainer/signal_connector.py
@@ -1,4 +1,3 @@
-import functools
 import logging
 import os
 import re
@@ -90,8 +89,6 @@ class _SignalConnector(_LightningSignalConnector):
             for signal_handler in auto_requeue_signals:
                 signals[signal_handler].append(self._lsf_sigusr_handler_fn)
 
-            self._lsf_patch_signals_for_workers(auto_requeue_signals)
-
         for signum, handlers in signals.items():
             if not handlers:
                 continue
@@ -102,51 +99,6 @@ class _SignalConnector(_LightningSignalConnector):
                 replace_existing=signum
                 in (signal.Signals.SIGUSR1, signal.Signals.SIGUSR2),
             )
-
-    def _lsf_patch_signals_for_workers(self, signals: list[signal.Signals]):
-        # LSF's `jsrun` seemingly propagates signals to all processes in the job,
-        # including subprocesses. This can lead to unexpected behavior when signals
-        # are also sent to other process than the main process, e.g., the data loader
-        # workers. To prevent this, we need to patch the signals for the worker processes.
-        # This is done by setting the signal handler to `SIG_IGN` for all signals that
-        # are used for auto-requeueing.
-
-        # We will do this by patching the `_prepare_dataloader` function of the trainer's
-        # _data_connector.
-
-        log.critical(
-            "Patching signal handlers for worker processes to prevent signal propagation (LSF)"
-        )
-        fn = self.trainer._data_connector._prepare_dataloader
-        """
-        Function signature:
-        (method) def _prepare_dataloader(
-            self,
-            dataloader: object,
-            shuffle: bool,
-            mode: RunningStage
-        ) -> object
-        """
-
-        def _patch_signals(_: int):
-            # Patch signal handlers for worker processes
-            for s in signals:
-                signal.signal(s, signal.SIG_IGN)
-
-        @functools.wraps(fn)
-        def _prepare_dataloader_patched(dataloader, shuffle, mode):
-            nonlocal signals
-
-            dataloader = fn(dataloader, shuffle, mode)
-
-            # Set the worker_init_fn to patch the signals
-            if isinstance(dataloader, torch.utils.data.DataLoader):
-                dataloader.worker_init_fn = _patch_signals
-
-            return dataloader
-
-        self.trainer._data_connector._prepare_dataloader = _prepare_dataloader_patched
-        log.info("Patched signal handlers for worker processes (LSF)")
 
     def _should_ignore_signal_handler(self) -> str | None:
         if threading.current_thread() is not threading.main_thread():
