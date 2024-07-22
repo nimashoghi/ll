14222d47a6daa9a47bdce766476ce9eb2482c56f
Nima Shoghi <nimashoghi@gmail.com>
2024-07-22 13:23:46 -0400

    refactor: Remove unused code in seed.py

--- src/ll/util/seed.py
+++ src/ll/util/seed.py
@@ -1,4 +1,3 @@
-from contextlib import contextmanager
 from logging import getLogger
 
 import lightning.fabric.utilities.seed as LS
@@ -10,17 +9,3 @@ def seed_everything(seed: int | None, *, workers: bool = False):
     seed = LS.seed_everything(seed, workers=workers)
     log.critical(f"Set global seed to {seed}.")
     return seed
-
-
-def reset_seed():
-    LS.reset_seed()
-    log.critical("Reset global seed.")
-
-
-@contextmanager
-def seed_context(seed: int | None, *, workers: bool = False):
-    try:
-        seed = seed_everything(seed, workers=workers)
-        yield
-    finally:
-        reset_seed()
